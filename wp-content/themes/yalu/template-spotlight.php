efe5efc141fb1b96472436666184b1205af96a3f
Bobbilee Hartman <bobbileehartman@gmail.com>
2013-08-14 08:50:06 -0500

    adding new features to the professional spotlight page

---
+++ wp-content/themes/yalu/template-spotlight.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Spotlight
+*/
+
+//get global prefix
+global $sr_prefix;
+
+//get template header
+get_header();
+
+
+?>
+
+            <div class="main-content">
+                
+                <?php
+            
+                     $query = new WP_Query(array(
+                         'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1 ),
+                         's' => get_query_var('s'),
+                         'post_type' => array('post'),
+                         'category_name' => "professional-spotlight"
+                     ) );    
+        
+                     
+                     if (!have_posts()) { 
+                         echo '<div class="nopost"><h3>'.__("No posts has been found!", 'sr_yalu_theme').'</h3></div>';
+                     }
+                ?>
+            
+                <?php
+                    get_template_part( 'includes/loop', 'blog');
+                ?>
+                
+                <?php sr_pagination(__('Previous', 'sr_yalu_theme'), __('Next', 'sr_yalu_theme')); ?>                
+                
+            </div>    
+        
+<?php get_footer(); ?>
